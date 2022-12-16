58c89bf577e6f21d240135031abbce91f774804c
syy6zn <syang2468@yahoo.com>
2022-12-16 12:58:48 -0500

    allow static files to be used on heroku

--- china_ocean/models.py
+++ china_ocean/models.py
@@ -25,7 +25,7 @@ class Item(models.Model):
         bottle = '20 oz.'
 
     name = models.TextField()
-    subtitle = models.TextField(null=True)
+    subtitle = models.TextField(null=True, blank=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     large_price = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
     quart_price = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
@@ -33,7 +33,7 @@ class Item(models.Model):
     menu_number = models.CharField(max_length=6, null=True, blank=True)
     lunch = models.CharField(max_length=4, choices=YesNo.choices, default=YesNo.no)
     dinner = models.CharField(max_length=4, choices=YesNo.choices, default=YesNo.no)
-    substitute = models.CharField(max_length=4, choices=YesNo.choices, default=YesNo.no)
+    substitute = models.CharField(max_length=4, choices=YesNo.choices, default=YesNo.no, null=True, blank=True)
     size = models.CharField(max_length=10, choices=Size.choices, null=True, blank=True)
     header = models.ForeignKey(Header, on_delete=models.CASCADE)
     spicy = models.CharField(max_length=4, choices=YesNo.choices, null=True, blank=True)
